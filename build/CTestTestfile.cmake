3ab5b35d541563dfd6839346fff42c45762bf222
Pripekashkin <densik554@gmail.com>
2020-04-11 15:13:09 +0300

    sshtest

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/noreply/ManipulatorMX106/src
+# Build directory: /home/noreply/ManipulatorMX106/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mx106_description)
---
+++ build/mx106_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noreply/ManipulatorMX106/src/mx106_description
+# Build directory: /home/noreply/ManipulatorMX106/build/mx106_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
